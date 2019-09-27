8f779ca774f46d5930de6a44e40b0a26205c53b8
Michael Lynch <michael@michaelrlynch.com>
2019-09-27 00:56:44 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,4 +11,15 @@ class Doctor
     @@all
   end
 
+  def new_appointment(patient, date)
+    Appointment.new(patient, date)
+  end
+
+  def appointments
+  end
+
+  def patients
+    Appointment.map(&:patient)
+  end    
+
 end
